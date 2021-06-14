0d6df4d8a61ac997542095e7ecd5563365e7fa66
Guillaume <82163402+Gourra@users.noreply.github.com>
2021-06-14 07:50:16 +0200

    comments

--- complete.php
+++ complete.php
@@ -1,32 +1,32 @@
 <?php
-    $db = new SQLite3('amazon2.db');
-    $c_user = $_COOKIE["user"];
-    $c_nom= $db->querySingle("SELECT nomclient FROM client WHERE mailclient='$c_user'");
-    $refclient= $db->querySingle("SELECT refclient FROM client WHERE mailclient='$c_user'");
-    $prod = $_GET['produit'];
-    $exist = $db->querySingle("SELECT refcommande FROM commande WHERE etat='Panier' AND refclient='$refclient' AND refarticle='$prod'");
+    $db = new SQLite3('amazon2.db'); //BDD
+    $c_user = $_COOKIE["user"]; // Récupération cookie connexion
+    $c_nom= $db->querySingle("SELECT nomclient FROM client WHERE mailclient='$c_user'"); //Requete SQL recup nom d'utilisateur
+    $refclient= $db->querySingle("SELECT refclient FROM client WHERE mailclient='$c_user'"); // Requete SQL recup refclient
+    $prod = $_GET['produit']; //Récupération de l'id produit via l'url
+    $exist = $db->querySingle("SELECT refcommande FROM commande WHERE etat='Panier' AND refclient='$refclient' AND refarticle='$prod'"); //Requete SQL voir si l'user a deja cet article dans le panier
 
-    if ($c_nom != "") {
+    if ($c_nom != "") { // Si user est connecté
 
-        if ($exist != "") {
-            $qte = $db->querySingle("SELECT qte FROM commande WHERE refcommande='$exist'");
-            $nqte = $qte + 1;
-            $update = "UPDATE commande SET qte='$nqte' WHERE refcommande='$exist'";
-            $exe = $db->exec($update);
-            header('Location: ..');
+        if ($exist != "") { //Et si la commande du produit est deja dans le panier 
+            $qte = $db->querySingle("SELECT qte FROM commande WHERE refcommande='$exist'"); //Récupération de la qte dans le panier
+            $nqte = $qte + 1; //Calcul de la nouvelle quantite a commander
+            $update = "UPDATE commande SET qte='$nqte' WHERE refcommande='$exist'"; // Requete SQL met a jour la quantitee 
+            $exe = $db->exec($update); //execution de la requete
+            header('Location: ..'); //redirection
         }else {
-            $date = date("d/m/Y");
-            $vendeur = $db->querySingle("SELECT refvendeur FROM Stock WHERE refarticle='$prod'");
-            CreateCommande($prod,$refclient,$vendeur,$date);
-            header('Location: ..');
+            $date = date("d/m/Y");//Si le produit n'est pas deja dans le panier du client
+            $vendeur = $db->querySingle("SELECT refvendeur FROM Stock WHERE refarticle='$prod'"); //Requete SQL vendeur du produit
+            CreateCommande($prod,$refclient,$vendeur,$date); // appel fonction creer une ligne 
+            header('Location: ..'); //redirection
         }  
     }else{
-        header('Location: /connexion.php');
+        header('Location: /connexion.php'); // Si user non connecté redirection vers connexion
     }
 
     function CreateCommande($refart,$refcl,$refve,$date){ // Creer une ligne commande dans la BDD
         $db = new SQLite3('amazon2.db'); // BDD concernée
-        $ref = GenRefClient(9); // Creation d'un id commande aléatoire 
+        $ref = GenRefClient(9); // Creation d'un id commande aléatoire appel fonction  
         $querry = "INSERT INTO commande(refcommande, refarticle, refclient, refvendeur, qte, dateachat, etat) VALUES ('$ref', '$refart', '$refcl', '$refve', 1, '$date', 'Panier')"; // Requete SQL qui creer une nouvelle ligne dans la table commande
         $send = $db->exec($querry); // Execute la requete
     }
