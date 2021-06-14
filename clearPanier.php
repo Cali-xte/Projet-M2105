0ccc852eb6da15a1dce050ce14672a0a14df507a
Guillaume <82163402+Gourra@users.noreply.github.com>
2021-06-14 07:39:26 +0200

    comments

--- clearPanier.php
+++ clearPanier.php
@@ -1,7 +1,7 @@
 <?php
-    $db = new SQLite3('amazon2.db');
-    $c_user = $_COOKIE["user"];
-    $querry = "DELETE FROM commande WHERE refclient=(SELECT refclient FROM client WHERE mailClient='$c_user') AND etat='Panier' ";
-    $send= $db->exec($querry);
-    header('Location: /panier.php');  
+    $db = new SQLite3('amazon2.db'); //Base de donnée
+    $c_user = $_COOKIE["user"]; //Récupération cookie de conexion
+    $querry = "DELETE FROM commande WHERE refclient=(SELECT refclient FROM client WHERE mailClient='$c_user') AND etat='Panier' "; //Requete SQL supprime "panier"
+    $send= $db->exec($querry); //execution de la requete
+    header('Location: /panier.php');  //redirection
 ?>
