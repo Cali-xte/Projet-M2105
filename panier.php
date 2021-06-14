60423b5244cfb9fd866ac0fb0c633ea9b3103390
Guillaume <82163402+Gourra@users.noreply.github.com>
2021-06-14 07:59:41 +0200

    comments

--- panier.php
+++ panier.php
@@ -11,9 +11,10 @@
     <title>Amazon&sup2; </title>
 </head>
 <?php
-    $db = new SQLite3('amazon2.db');
-    $c_user = $_COOKIE["user"];
-    $c_nom= $db->querySingle("SELECT nomclient FROM client WHERE mailclient='$c_user'");
+    $db = new SQLite3('amazon2.db'); //BDD
+    $c_user = $_COOKIE["user"]; //Récupération cookie authentification
+    //Requetes utiles
+    $c_nom= $db->querySingle("SELECT nomclient FROM client WHERE mailclient='$c_user'"); 
     $nbarticle = $db->querySingle("SELECT count(*) FROM commande WHERE refclient=(SELECT refclient FROM client WHERE mailClient='$c_user') AND etat='Panier' ");
     $article = $db->query("SELECT * FROM article WHERE refarticle IN(SELECT refarticle FROM commande WHERE refclient=(SELECT refclient FROM client WHERE mailClient='$c_user') AND etat='Panier') ");
     $refclient= $db->querySingle("SELECT refclient FROM client WHERE mailclient='$c_user'");
@@ -37,32 +38,34 @@
         </a>
     </header>
     <?php
-        if ($c_nom == '') {
+        if ($c_nom == '') { //Si l'utilisateur n'est pas connecté le panier ne s'affiche pas 
             echo "<p class='msgnon'>Connectez vous pour accéder à votre panier</p>";
         }else{
-            $totalht = 0;
+            $totalht = 0; //Instanciation du prix hors taxes
+            
+            //Affichage du panier
             echo "<div class='parent'>";
             echo "<h1>Panier de $c_nom</h1>";
             $articletot = 0;
             while ($row = $article->fetchArray()) {
                 echo "<div class='child'>";
-                echo "<img src='assets/produits/{$row[refarticle]}.png' alt='Image Produit' class='imgproduit'>";
-                echo "<div class='prodNom'>{$row[marque]} {$row[nomarticle]}</div>";
-                $qte = $db->querySingle("SELECT qte FROM commande WHERE refarticle='$row[refarticle]' AND refclient='$refclient'");
-                echo "Qte : x$qte";
-                echo "<div class='prodPrix'>{$row[prix]} €</div>";
+                echo "<img src='assets/produits/{$row[refarticle]}.png' alt='Image Produit' class='imgproduit'>"; //Affiche l'image
+                echo "<div class='prodNom'>{$row[marque]} {$row[nomarticle]}</div>"; //Afiche nom + marque
+                $qte = $db->querySingle("SELECT qte FROM commande WHERE refarticle='$row[refarticle]' AND refclient='$refclient'"); //Requete SQL récupère la qte dans le panier de chaque article
+                echo "Qte : x$qte"; //Affichage de la quantite dans le panier
+                echo "<div class='prodPrix'>{$row[prix]} €</div>"; //Affiche le prix
                 echo "</div>";
-                $articletot += $qte;
-                $totalht += ($row['prix']*$qte);
+                $articletot += $qte; //Calcule le nombre d'article total
+                $totalht += ($row['prix']*$qte); //Calcul du prix total en fonction de l'article et de sa quantitee
             }
-            $tva = $totalht*0.20;
-            $total = $totalht + $tva;
+            $tva = $totalht*0.20; // Calcul TVA (20%)
+            $total = $totalht + $tva; //Prix total
             //echo "Prix HT : $totalht €<br>";
             //echo "TVA : $tva €<br>";
-            echo "<p>Article dans le panier : $articletot</p>";
-            echo "<p class='total'>Total (TVA comprise) : $total €<br><br></p>";
-            echo "<div class='decision' action='clearPanier.php'><input type='button' value='Acheter' id='buy'>";
-            echo "<a href='clearPanier.php'><input type='button' value='Vider Panier' id='clear' ></a></div>";
+            echo "<p>Article dans le panier : $articletot</p>"; //Afiche nb article total 
+            echo "<p class='total'>Total (TVA comprise) : $total €<br><br></p>"; // Prix a payer
+            echo "<div class='decision' action='clearPanier.php'><input type='button' value='Acheter' id='buy'>"; //bouton acheter ! ne fonctionne pas ! -> utilisable ensuite
+            echo "<a href='clearPanier.php'><input type='button' value='Vider Panier' id='clear' ></a></div>"; //bouton pour vider le panier
             echo "</div>";
         }
 
